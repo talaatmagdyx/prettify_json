dfcef0bf4248ddbb9cb8032312829ac0f0ae0d43
talaatmagdyx <talaatmagdy75@gmail.com>
2024-05-25 05:23:48 +0300

    init commit

---
+++ lib/prettify_json.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prettify_json/version"
+
+module PrettifyJson
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prettify_json/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PrettifyJson
+  VERSION = "0.1.0"
+end
